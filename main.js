d8a4d4f1e51780deb0718e0fbbc93f305f4ac9fb
mskk0803 <toyama2ura@gmail.com>
2021-07-20 08:42:45 +0900

    portforio

---
+++ main.js
@@ -0,0 +1,6 @@
+const about = document.getElementById("about");
+const charactor = document.getElementById("charactor");
+const hobbies = document.getElementById("hobbies");
+const likes = document.getElementById("likes");
+const dislikes = document.getElementById("dislikes");
+const skill = document.getElementById("skill");
