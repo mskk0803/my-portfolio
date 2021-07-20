d8622874057254a8d1d1b67a4c4290a2884a8dde
mskk0803 <toyama2ura@gmail.com>
2021-07-20 12:08:15 +0900

    html

--- main.js
+++ main.js
@@ -4,3 +4,22 @@ const hobbies = document.getElementById("hobbies");
 const likes = document.getElementById("likes");
 const dislikes = document.getElementById("dislikes");
 const skill = document.getElementById("skill");
+
+about.onclick = function () {
+  alert.log("東京農工大学修士1年");
+};
+charactor.onclick = function () {
+  alert("まじめ");
+};
+hobbies.onclick = function () {
+  alert("散歩");
+};
+likes.onclick = function () {
+  alert("お酒");
+};
+dislikes.onclick = function () {
+  alert("なめくじ");
+};
+skill.onclick = function () {
+  alert("普通自動車運転免許");
+};
