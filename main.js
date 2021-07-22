94c117f4f3b7152e26f7a4861b86299e5d837e6c
mmskk0803 <toyama2ura@gmail.com>
2021-07-22 15:51:51 +0900

    cssおわり

--- main.js
+++ main.js
@@ -1,25 +1,8 @@
+const myname = document.getElementById("myname");
 const about = document.getElementById("about");
-const charactor = document.getElementById("charactor");
-const hobbies = document.getElementById("hobbies");
-const likes = document.getElementById("likes");
-const dislikes = document.getElementById("dislikes");
+const hobby = document.getElementById("hobby");
+const favorite = document.getElementById("favorite");
 const skill = document.getElementById("skill");
+const link = document.getElementById("skill");
 
-about.onclick = function () {
-  alert("東京農工大学修士1年");
-};
-charactor.onclick = function () {
-  alert("まじめ");
-};
-hobbies.onclick = function () {
-  alert("散歩");
-};
-likes.onclick = function () {
-  alert("お酒");
-};
-dislikes.onclick = function () {
-  alert("なめくじ");
-};
-skill.onclick = function () {
-  alert("普通自動車運転免許");
-};
+myname.onmouseover = function () {};
