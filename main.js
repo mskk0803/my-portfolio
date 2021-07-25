a01792f4bfc2b5e181087f09caf0f14ef7145b86
mskk0803 <toyama2ura@gmail.com>
2021-07-25 19:27:01 +0900

    num

--- main.js
+++ main.js
@@ -13,41 +13,65 @@ const favorite = document.getElementById("favorite");
 const skill = document.getElementById("skill");
 const link = document.getElementById("link");
 
+let nummyname = 0;
 myname.onmouseover = function () {
-  const mynameBox = document.getElementById("myname-box");
-  mynameBox.animate([{ opacity: 0 }, { opacity: 1 }], 1000);
-  mynameBox.innerHTML = "<p>松浦早希子</p><p>Matsuura SAKIKO</p>";
+  if (nummyname === 0) {
+    const mynameBox = document.getElementById("myname-box");
+    mynameBox.animate([{ opacity: 0 }, { opacity: 1 }], 1000);
+    mynameBox.innerHTML = "<p>松浦早希子</p><p>Matsuura SAKIKO</p>";
+    nummyname += 1;
+  }
 };
 
+let numabout = false;
 about.onmouseover = function () {
-  const aboutBox = document.getElementById("about-box");
-  aboutBox.animate([{ opacity: 0 }, { opacity: 1 }], 1000);
-  aboutBox.innerHTML = "<p>富山県富山市出身</p><p>東京農工大学大学院1年</p>";
+  if (numabout === false) {
+    const aboutBox = document.getElementById("about-box");
+    aboutBox.animate([{ opacity: 0 }, { opacity: 1 }], 1000);
+    aboutBox.innerHTML = "<p>富山県富山市出身</p><p>東京農工大学大学院1年</p>";
+    numabout = true;
+  }
 };
 
+let numhobby = false;
 hobby.onmouseover = function () {
-  const hobbyBox = document.getElementById("hobby-box");
-  hobbyBox.animate([{ opacity: 0 }, { opacity: 1 }], 1000);
-  hobbyBox.innerHTML = "<p>居酒屋巡り</p><p>散歩</p>";
+  if (numhobby === false) {
+    const hobbyBox = document.getElementById("hobby-box");
+    hobbyBox.animate([{ opacity: 0 }, { opacity: 1 }], 1000);
+    hobbyBox.innerHTML = "<p>居酒屋巡り</p><p>散歩</p>";
+    numhobby = true;
+  }
 };
 
+let numfavorite = false;
 favorite.onmouseover = function () {
-  const favoriteBox = document.getElementById("favorite-box");
-  favoriteBox.animate([{ opacity: 0 }, { opacity: 1 }], 1000);
-  favoriteBox.innerHTML = "<p>酒類全般</p><p>奇麗な風景</p>";
+  if (numfavorite === false) {
+    const favoriteBox = document.getElementById("favorite-box");
+    favoriteBox.animate([{ opacity: 0 }, { opacity: 1 }], 1000);
+    favoriteBox.innerHTML = "<p>酒類全般</p><p>奇麗な風景</p>";
+    numfavorite = true;
+  }
 };
 
+let numskill = false;
 skill.onmouseover = function () {
-  const skillBox = document.getElementById("skill-box");
-  skillBox.animate([{ opacity: 0 }, { opacity: 1 }], 1000);
-  skillBox.innerHTML =
-    "<p>普通自動車運転免許</p><p>秘書技能検定2級</p><p>Rails/HTML/CSS</p>";
+  if (numskill === false) {
+    const skillBox = document.getElementById("skill-box");
+    skillBox.animate([{ opacity: 0 }, { opacity: 1 }], 1000);
+    skillBox.innerHTML =
+      "<p>普通自動車運転免許</p><p>秘書技能検定2級</p><p>Rails/HTML/CSS</p>";
+    numskill = true;
+  }
 };
 
+let numlink = false;
 link.onmouseover = function () {
-  const linkBox = document.getElementById("link-box");
-  linkBox.style.visibility = "visible";
-  linkBox.animate([{ opacity: 0 }, { opacity: 1 }], 1000);
+  if (numlink === false) {
+    const linkBox = document.getElementById("link-box");
+    linkBox.style.visibility = "visible";
+    linkBox.animate([{ opacity: 0 }, { opacity: 1 }], 1000);
+    numlink = true;
+  }
 };
 
 // gallrey
